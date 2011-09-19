06e7c879ec56bae57e53e5eb2f90e1062346460c
Patrick Mylund Nielsen <patrick@patrickmylund.com>
2011-09-19 16:35:04 +0200

    gofmt

--- src/picugen.go
+++ src/picugen.go
@@ -23,45 +23,45 @@ import (
 )
 
 const (
-	version = "1.0"
+	version   = "1.0"
 	chunkSize = 32768 // Read files in 32KB chunks
 )
 
 var (
-	alg *string = flag.String("a", "md5", "algorithm")
-	str *string = flag.String("s", "", "string to hash instead of file")
-	key *string = flag.String("k", "", "key (for hashes that use a key, e.g. HMAC)")
+	alg  *string = flag.String("a", "md5", "algorithm")
+	str  *string = flag.String("s", "", "string to hash instead of file")
+	key  *string = flag.String("k", "", "key (for hashes that use a key, e.g. HMAC)")
 	salt *string = flag.String("salt", "", "salt")
 )
 
 var (
 	algDescs = map[string]string{
-		"adler32": "Adler-32 checksum (RFC 1950)",
-		"crc32": "32-bit cyclic redundancy check (CRC-32) checksum (defaults to IEEE polynomial)",
-		"crc32ieee": "CRC-32 using the IEEE polynomial (0xedb88320)",
+		"adler32":         "Adler-32 checksum (RFC 1950)",
+		"crc32":           "32-bit cyclic redundancy check (CRC-32) checksum (defaults to IEEE polynomial)",
+		"crc32ieee":       "CRC-32 using the IEEE polynomial (0xedb88320)",
 		"crc32castagnoli": "CRC-32 using the Castagnoli polynomial (0x82f63b78)",
-		"crc32koopman": "CRC-32 using the Koopman polynomial (0xeb31d82e)",
-		"crc64": "64-bit cyclic redundancy check (CRC-64) checksum (defaults to ISO polynomial)",
-		"crc64iso": "CRC-64 using the ISO polynomial (0xD800000000000000)",
-		"crc64ecma": "CRC-64 using the ECMA polynomial (0xC96C5795D7870F42)",
-		"fnv": "FNV-1 non-cryptographic hash (defaults to fnv32)",
-		"fnv32": "32-bit FNV-1",
-		"fnv32a": "32-bit FNV-1a",
-		"fnv64": "64-bit FNV-1",
-		"fnv64a": "64-bit FNV-1a",
-		"hmac": "Keyed-Hash Message Authentication Code (HMAC) (requires -k <key>) (defaults to SHA-256)",
-		"hmacmd5": "HMAC using MD5 (requires -k <key>)",
-		"hmacsha1": "HMAC using SHA-1 (requires -k <key>)",
-		"hmacsha256": "HMAC using SHA-256 (requires -k <key>)",
-		"hmacsha512": "HMAC using SHA-512 (requires -k <key>)",
-		"md4": "MD4 hash (RFC 1320)",
-		"md5": "MD5 hash (RFC 1321)",
-		"ripemd160": "RIPEMD-160 hash",
-		"sha1": "SHA-1 hash (RFC 3174)",
-		"sha224": "SHA-224 hash (FIPS 180-2)",
-		"sha256": "SHA-256 hash (FIPS 180-2)",
-		"sha384": "SHA-384 hash (FIPS 180-2)",
-		"sha512": "SHA-512 hash (FIPS 180-2)",
+		"crc32koopman":    "CRC-32 using the Koopman polynomial (0xeb31d82e)",
+		"crc64":           "64-bit cyclic redundancy check (CRC-64) checksum (defaults to ISO polynomial)",
+		"crc64iso":        "CRC-64 using the ISO polynomial (0xD800000000000000)",
+		"crc64ecma":       "CRC-64 using the ECMA polynomial (0xC96C5795D7870F42)",
+		"fnv":             "FNV-1 non-cryptographic hash (defaults to fnv32)",
+		"fnv32":           "32-bit FNV-1",
+		"fnv32a":          "32-bit FNV-1a",
+		"fnv64":           "64-bit FNV-1",
+		"fnv64a":          "64-bit FNV-1a",
+		"hmac":            "Keyed-Hash Message Authentication Code (HMAC) (requires -k <key>) (defaults to SHA-256)",
+		"hmacmd5":         "HMAC using MD5 (requires -k <key>)",
+		"hmacsha1":        "HMAC using SHA-1 (requires -k <key>)",
+		"hmacsha256":      "HMAC using SHA-256 (requires -k <key>)",
+		"hmacsha512":      "HMAC using SHA-512 (requires -k <key>)",
+		"md4":             "MD4 hash (RFC 1320)",
+		"md5":             "MD5 hash (RFC 1321)",
+		"ripemd160":       "RIPEMD-160 hash",
+		"sha1":            "SHA-1 hash (RFC 3174)",
+		"sha224":          "SHA-224 hash (FIPS 180-2)",
+		"sha256":          "SHA-256 hash (FIPS 180-2)",
+		"sha384":          "SHA-384 hash (FIPS 180-2)",
+		"sha512":          "SHA-512 hash (FIPS 180-2)",
 	}
 )
 
@@ -129,7 +129,7 @@ func HashFile(gen hash.Hash, f io.Reader) (string, os.Error) {
 	for {
 		bytesRead, err := f.Read(buf)
 		if err != nil {
-			if bytesRead == 0 && err == os.EOF { // Empty file
+			if err == os.EOF && bytesRead == 0 { // Empty file
 				gen.Write([]byte(""))
 			} else {
 				return "", err
@@ -195,6 +195,6 @@ func main() {
 			gen.Reset()
 		}
 	} else { // Hash string
-		fmt.Println(HashString(gen, *salt + *str))
+		fmt.Println(HashString(gen, *salt+*str))
 	}
 }
